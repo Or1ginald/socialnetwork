a16ed9e4de4624f666ead7be8aaee942b046455b
Ivan Vorobev <superhd@icloud.com>
2021-07-14 19:39:46 +0300

    add messages block

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import a from './App.module.css';
 import Navigation from "./components/Navigation/Navigation";
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profile/Profile";
+// import Messages from "./components/MainContent/Messages";
 
 function App() {
     return (
         <div className="App">
             <div className={a.appWrapper}>
-            <Navigation/>
-            <Profile/>
+                <Navigation/>
+                <div className={a.content}>
+                    <Profile/>
+                    {/*<Messages />*/}
+                </div>
             </div>
         </div>
     );
--- src/components/Profile/Profile/Profile.tsx
+++ src/components/Profile/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import PictureBlock from "./PictureBlock/PictureBlock";
-import InformationBlock from "./InformationBlock";
+import PictureBlock from "../PictureBlock/PictureBlock";
+import InformationBlock from "../InformationBlock/InformationBlock";
 import p from "./Profile.module.css"
 
 function Profile() {
